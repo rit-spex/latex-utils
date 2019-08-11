859f3bcd75ae52e2298b307aaf9b60d6d0904af4
EvanPutnam <emput1998@gmail.com>
2019-08-11 19:01:04 -0400

    Adding a .bat file for a automatic install of MikTex

---
+++ Install_Scripts/install_miktex.bat
@@ -0,0 +1,5 @@
+echo "Starting installation"
+miktexsetup --package-set=basic download
+echo "Please wait...  This may take a while."
+miktexsetup install
+echo "Done installing, please check if MikTex is installed."
